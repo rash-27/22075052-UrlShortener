6e9d407d3d8465404b9838422368177363d87aa7
Rashmik <rashmik2705@gmail.com>
2023-10-25 10:32:33 +0530

    created a model

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class UrlShortner(models.Model):
+    long_url = models.URLField(primary_key=True)
+    short_code = models.CharField(unique=True,max_length=300)
+
